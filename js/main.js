dc607ecbd7ac545a9879af767f71d0f4be82dd67
Derek <38119240+DerekJB1987@users.noreply.github.com>
2018-08-21 18:34:12 -0700

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//fetching HTML Elements in variables by ID
+var x = document.getElementById("form_sample");
+ 
+var createform = document.createElement('form'); // Create New Element form
+ createform.setAttribute("action", ""); // Setting action Attribute on form
+ createform.setAttribute("method", "post"); // Setting method Attribute on form
+ x.appendChild(createform);
+ 
+var heading = document.createElement('h2'); // Heading of form
+ heading.innerHTML = "Contact Form ";
+ createform.appendChild(heading);
+ 
+var line = document.createElement('hr'); //giving horizontal row after heading
+ createform.appendChild(line);
+ 
+var linebreak = document.createElement('br');
+ createform.appendChild(linebreak);
+ 
+var namelabel = document.createElement('label'); // Create Label for name field
+ namelabel.innerHTML = "Your Name : "; // Set Field Labels
+ createform.appendChild(namelabel);
+ 
+var inputelement = document.createElement('input'); // Create input field for name
+ inputelement.setAttribute("type", "text");
+ inputelement.setAttribute("name", "dname");
+ createform.appendChild(inputelement);
+ 
+var linebreak = document.createElement('br');
+ createform.appendChild(linebreak);
