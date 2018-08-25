cf37336111ea1975e80e07428f67e6ac702b1709
Derek <benakderek@seattleu.edu>
2018-08-25 00:22:10 +0000

    entered some JavaScript

--- js/main.js
+++ js/main.js
@@ -1,44 +1,53 @@
 //fetching HTML Elements in variables by ID
 /*eslint-disable no-empty-label, no-redeclare*/
-var x = document.getElementById("form_sample");
- 
-var createform = document.createElement('form'); // Create New Element form
- createform.setAttribute("action", ""); // Setting action Attribute on form
- createform.setAttribute("method", "post"); // Setting method Attribute on form
- x.appendChild(createform);
- 
-var heading = document.createElement('h2'); // Heading of form
- heading.innerHTML = "Contact Form ";
- createform.appendChild(heading);
- 
-var line = document.createElement('hr'); //giving horizontal row after heading
- createform.appendChild(line);
- 
-var linebreak = document.createElement('br');
- createform.appendChild(linebreak);
- 
-var namelabel = document.createElement('label'); // Create Label for name field
- namelabel.innerHTML = "Your Name : "; // Set Field Labels
- createform.appendChild(namelabel);
- 
-var inputelement = document.createElement('input'); // Create input field for name
- inputelement.setAttribute("type", "text");
- inputelement.setAttribute("name", "dname");
- createform.appendChild(inputelement);
- 
-var linebreak = document.createElement('br');
- createform.appendChild(linebreak);
- 
-var emaillabel = document.createElement('label'); //Create Label for email field
- emaillabel.innerHTML = "Your Email : ";
- createform.appendChild(emaillabel);
- 
-var emailelement = document.createElement('input'); // Create input field for email
- emailelement.setAttribute("type", "text");
- emailelement.setAttribute("name", "demail");
- createform.appendChild(emailelement);
- 
-var emailbreak = document.createElement('br');
- createform.appendChild(emailbreak);
- 
- 
+
+function form()                                   
+{
+    var name = document.forms["ContactForm"]["Name"];    
+    var address =  document.forms["ContactForm"]["Mail"]; 
+    var email = document.forms["ContactForm"]["EMail"];   
+    var phone = document.forms["ContactForm"]["Phone"]; 
+    var username - document.forms["ContactForm"]["Username"];
+    var password = document.forms["ContactForm"]["Password"]; 
+    
+    if (name.value == "")                                 
+        {
+            window.alert("Please enter your name.");
+            name.focus();
+            return false;
+        }
+    if (address.value == "")                              
+    {
+        window.alert("Please enter your address.");
+        name.focus();
+        return false;
+    }
+      
+    if (email.value == "")                                  
+    {
+        window.alert("Please enter a valid e-mail address.");
+        email.focus();
+        return false;
+    }
+    
+    if (phone.value == "")                          
+    {
+        window.alert("Please enter your telephone number.");
+        phone.focus();
+        return false;
+    }
+    
+    if (username.value == "")
+    {
+        
+        window.alert('Please enter your username.');
+        username.focus();
+        return false;
+    }
+    
+    if (password.value == "")                       
+    {
+        window.alert("Please enter your password");
+        password.focus();
+        return flase;
+    }
