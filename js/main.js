3d2d235362d94ed7030191040db5e9afee14663c
Derek <benakderek@seattleu.edu>
2018-08-29 00:43:11 +0000

    updated JS file with missing quotes

--- js/main.js
+++ js/main.js
@@ -4,27 +4,27 @@ console.log('It works');
 
 /*Prompt visitor to enter information*/
 
-    var name = document.getElementById(Name").value;
+    var name = document.getElementById("Name").value;
     var at = document.getElementById("email").value.indexOf("@");
-    var phone = document.getElementById(phone").value;
+    var phone = document.getElementById("phone").value;
     submitOk = "true";
     
     if (name.length > 20) {
         alert("The name may have no more than 20 characters");
-        submitOK = "false";
+        submit = "false";
     }
     
-    if (at == -1) {
+    if (at === -1) {
         alert("Not a valid e-mail!");
-        submitOK = "false";
+        submit = "false";
     }
     
     if (phone.length > 10) {
         alert("The name may have no more than 10 characters");
-        submitOK = "false";
+        submit = "false";
     }    
 
-    if (submitOK == "false") {
+    if (submit === "false") {
       return false;
     }
 
@@ -34,5 +34,5 @@ let contact = document.querySelector('#contactform');
 
 contact.addEventListener("submit", function (event) {
     event.preventDefault();
-    validateForm();
+    validatemyorm();
   });
