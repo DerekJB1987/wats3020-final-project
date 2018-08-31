53764e35cc3a889415375948e73411cbfbdc4e12
Derek <benakderek@seattleu.edu>
2018-08-31 02:24:00 +0000

    finalizing validation

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ let contact = document.querySelector('#myform');
 
 contact.addEventListener("submit", function (event) {
     event.preventDefault();
+
+
     let config={
         name: {
             required: true
@@ -22,8 +24,14 @@ contact.addEventListener("submit", function (event) {
         }
     };
     let validator= new Validator(config);
-    validator.validateForm();
-    window.location="received.html"
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let phone = document.querySelector('#phone').value;
+//    validator.validateForm();
+    validator.validateName(name);
+    validator.validateEmail(email);
+    validator.validatePhone(phone);
+//    window.location="received.html"
     });
     
 
@@ -33,15 +41,43 @@ contact.addEventListener("submit", function (event) {
 class Validator {
     constructor(config){
         this.config=config;
+        this.namevalid=true;
+        this.emailvalid=true;
+        this.phonevalid=true;
     }
     
-    validateForm(){
-        console.log('validate');
-    }
+//    validateForm(){
+//        console.log('validate');
+//    }
    
-    validateName(){}
-    validateEmail(){}
-    validatePhone(){}
+    validateName(testName){
+        console.log(testName)
+        if (this.config.name.required === true) {
+            if (testName.length ===0){
+                this.nameValid=false;
+            } else this.nameValid=true;
+        }
+        console.log('nameValid',this.nameValid)
+    }
+
+    validateEmail(testEmail){
+        console.log(testEmail)
+        if (this.config.email.required === true) {
+            if (testEmail.length ===0){
+                this.emailValid=false;
+            } else this.emailValid=true;
+        }
+        console.log('emailValid',this.emailValid)
+    }
+    
+    validatePhone(testPhone){
+        console.log(testPhone)
+        if (this.config.phone.required === true) {
+            if (testPhone.length ===0){
+                this.phoneValid=false;
+            } else this.phoneValid=true;
+        }
+    } console.log('phoneValid',this.phoneValid)
 }
 
 /*declaring config variables: name, email, and phone*/
