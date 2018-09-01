54d48b027d7d86e755a3838b38e4ebc3cbcbd4ea
Derek Benak <benak.derek@gmail.com>
2018-08-31 17:58:57 -0700

    JavaScript file updates
    fixing error messages function

---
+++ main.js
@@ -0,0 +1,106 @@
+
+
+console.log('It works');
+/*Event listener to handle form submission*/
+let contact = document.querySelector('#myform');
+
+
+contact.addEventListener("submit", function (event) {
+    event.preventDefault();
+/*Event listener to handle form submission*/
+
+    let config={
+        name: {
+            required: true
+        },
+
+        email: {
+            required: true,
+        },
+
+        phone: {
+            required: true,
+        }
+    };
+
+    let validator= new Validator(config);
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let phone = document.querySelector('#phone').value;
+   validator.validateForm();
+    validator.validateName(name);
+        if (validator.namevalid === false){
+            document.querySelector('#name')
+        }
+    validator.validateEmail(email);
+        if (validator.emailvalid === false){
+            document.querySelector('#email')
+        }
+    validator.validatePhone(phone);
+        if (validator.phonevalid === false){
+            document.querySelector('#phone')
+        }
+   window.location="received.html"
+    });
+
+
+/*Validator class declaring name, email, phone objects*/
+
+class Validator {
+    constructor(config){
+        this.config=config;
+        this.namevalid=true;
+        this.emailvalid=true;
+        this.phonevalid=true;
+    };
+    /*Error messages  */
+    (function(){
+      var errors = {
+        name: ['Name is required, Please enter a name',]
+        email: ['A valid email address is required'],
+        phone: ['A phone number is required']
+      };
+
+   validateForm(){
+       console.log('validate');
+   }
+
+/*Function to remove error messages  */
+errorPlacement: function (err, element) {
+  err.addClass(classes[element.attr('name')])
+  err.insertBefore(element);
+},
+submitHandler: function(form) {
+  form.submit();
+}
+
+/*Form validation for name object*/
+    validateName(testName){
+        console.log(testName)
+        if (this.config.name.required === true) {
+            if (testName.length ===0){
+                this.nameValid=false;
+            } else this.nameValid=true;
+        }
+        console.log('nameValid',this.nameValid)
+    }
+/*Form validation for email object*/
+    validateEmail(testEmail){
+        console.log(testEmail)
+        if (this.config.email.required === true) {
+            if (testEmail.length ===0){
+                this.emailValid=false;
+            } else this.emailValid=true;
+        }
+        console.log('emailValid',this.emailValid)
+    }
+/*Form validation for phone object*/
+    validatePhone(testPhone){
+        console.log(testPhone)
+        if (this.config.phone.required === true) {
+            if (testPhone.length ===0){
+                this.phoneValid=false;
+            } else this.phoneValid=true;
+        } console.log('phoneValid',this.phoneValid)
+    }
+}
