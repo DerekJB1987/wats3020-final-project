4d895558d54aeecaf2dd320ed26c5ecd7f8636b8
Derek <38119240+DerekJB1987@users.noreply.github.com>
2018-09-03 19:12:58 -0700

    main.js
    trying to get my project to work

--- js/main.js
+++ js/main.js
@@ -1,47 +1,37 @@
-
-
 console.log('It works');
 /*Event listener to handle form submission*/
 let contact = document.querySelector('#myform');
 
-
 contact.addEventListener("submit", function (event) {
     event.preventDefault();
-/*Event listener to handle form submission*/
 
-let config={
-    name: {
-        required: true
-    },
 
-    email: {
-        required: true,
-    },
-
-    phone: {
-        required: true,
-    }
-};
-
-let validator= new Validator(config);
-let name = document.querySelector('#name').value;
-let email = document.querySelector('#email').value;
-let phone = document.querySelector('#phone').value;
-   validator.validateForm();
-    validator.validateName(name);
-        if (validator.namevalid === false){
-            document.querySelector('#name')
+    let config={
+        name: {
+            required: true
+        },
+        
+        email: {
+            required: true,
+            email: true
+        },
+        
+        phone: {
+            required: true,
+            minlength: 10
         }
-    validator.validateEmail(email);
-        if (validator.emailvalid === false){
-            document.querySelector('#email')
-        }
-    validator.validatePhone(phone);
-        if (validator.phonevalid === false){
-            document.querySelector('#phone')
-        }
-   window.location="received.html"
+    };
+    let validator= new Validator(config);
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let phone = document.querySelector('#phone').value;
+        validator.validateForm();
+        validator.validateName(name);
+        validator.validateEmail(email);
+        validator.validatePhone(phone);
+        window.location="received.html"
     });
+    
 
 
 /*Validator class declaring name, email, phone objects*/
@@ -52,41 +42,40 @@ class Validator {
         this.namevalid=true;
         this.emailvalid=true;
         this.phonevalid=true;
-      }
+    }
+    
     validateForm(){
-       console.log('validate');
-     }
+        console.log('validate');
    }
- }
-
-
 /*Form validation for name object*/
-validateName(testName){
-    console.log(testName)
-    if (this.config.name.required === true) {
-        if (testName.length ===0){
-            this.nameValid=false;
-      } else this.nameValid=true;
+    validateName(testName){
+        console.log(testName)
+        if (this.config.name.required === true) {
+            if (testName.length ===0){
+                this.nameValid=false;
+            } else this.nameValid=true;
+        }
+        console.log('nameValid',this.nameValid)
     }
-    console.log('nameValid',this.nameValid)
-  }
 /*Form validation for email object*/
-validateEmail(testEmail){
-    console.log(testEmail)
-    if (this.config.email.required === true) {
-        if (testEmail.length ===0){
-            this.emailValid=false;
-      } else this.emailValid=true;
+
+    validateEmail(testEmail){
+        console.log(testEmail)
+        if (this.config.email.required === true) {
+            if (testEmail.length ===0){
+                this.emailValid=false;
+            } else this.emailValid=true;
+        }
+        console.log('emailValid',this.emailValid)
     }
-    console.log('emailValid',this.emailValid)
-}
 /*Form validation for phone object*/
-validatePhone(testPhone){
-    console.log(testPhone)
-    if (this.config.phone.required === true) {
-        if (testPhone.length ===0){
+    
+    validatePhone(testPhone){
+        console.log(testPhone)
+        if (this.config.phone.required === true) {
+            if (testPhone.length ===0){
             this.phoneValid=false;
-        } else this.phoneValid=true;
-    } console.log('phoneValid',this.phoneValid)
-}
+            }   else this.phoneValid=true;
+        } console.log('phoneValid',this.phoneValid)
+    }
 }
